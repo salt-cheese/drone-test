70ff2c3b68450d5ab2495896b3dc9c67fdee9901
muhtarh <muhtar.hartopo@gdplabs.id>
2020-02-16 10:34:19 +0700

    init repo

---
+++ src/main/java/com/muhtar/DroneTest.java
@@ -0,0 +1,11 @@
+package com.muhtar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTest {
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTest.class, args);
+    }
+}
---
+++ src/main/java/com/muhtar/dummy/TestController.java
@@ -0,0 +1,13 @@
+package com.muhtar.dummy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "Hello world !";
+    }
+}
