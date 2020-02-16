a4a9c02c5c08059a89ce24f4da97328cfbf21b69
muhtarh <muhtar.hartopo@gdplabs.id>
2020-02-16 11:52:01 +0700

    check update pr

--- src/main/java/com/muhtar/dummy/TestController.java
+++ src/main/java/com/muhtar/dummy/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
     @GetMapping
     public String test(@RequestParam String name) {
         if (name != null && name.length() > 0) {
-            return "Hello " + name;
+            return "Hello " + name + ":)";
         } else {
             return "Hello world !";
         }
