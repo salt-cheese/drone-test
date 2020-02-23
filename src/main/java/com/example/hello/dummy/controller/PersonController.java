5e5d5c1461f5323ddf4b72c6eb6394877875fa56
muhtarh <muhtar.hartopo@gdplabs.id>
2020-02-23 13:18:09 +0700

    add integration test

--- src/main/java/com/example/hello/dummy/controller/PersonController.java
+++ src/main/java/com/example/hello/dummy/controller/PersonController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/persons")
 @RequiredArgsConstructor
 public class PersonController {
 
--- src/test/java/com/example/hello/dummy/PersonControllerIT.java
+++ src/test/java/com/example/hello/dummy/PersonControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,12 +28,27 @@ public class PersonControllerIT {
     public void findAll_Ok() {
 
         ResponseEntity<List<Person>> responseEntity = restTemplate.exchange(
-            "/person",
+            "/persons",
             HttpMethod.GET,
             HttpEntity.EMPTY,
             new ParameterizedTypeReference<List<Person>>() {}
          );
         Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         Assert.assertEquals(5, responseEntity.getBody().size());
+    }
+
+    @Test
+    public void findById_Ok() {
+        long id = 5L;
+        ResponseEntity<Optional<Person>> responseEntity = restTemplate.exchange(
+            "/persons/" + id,
+            HttpMethod.GET,
+            HttpEntity.EMPTY,
+            new ParameterizedTypeReference<Optional<Person>>() {}
+        );
+        String expectedName = "Naruto";
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assert.assertTrue(responseEntity.getBody().isPresent());
+        Assert.assertEquals(expectedName, responseEntity.getBody().get().getName());
     }
 }
